a97e481026aea46add363ec3d97f956c3df6e3c2
Jodex Saint-Vil <jsaintvi@aol.com>
2015-08-16 00:47:06 -0400

    Started with the profile page by adding a user controller with a show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
+
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
+
+  resources :users, only: [:show, :index]
+
+  #get 'users/:id' => "users#show", as: :profile
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
